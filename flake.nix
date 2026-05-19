5ab12df88053d7fe594a3b3f0524b8543dc0ded3
y0sif <yosifsoli981@gmail.com>
2026-05-19 07:52:00 +0300

    release v0.1.13

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "whisrs";
-          version = "0.1.12";
+          version = "0.1.13";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
